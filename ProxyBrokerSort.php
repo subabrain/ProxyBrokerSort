69c875cad1ee1986443d46366d328b6981d2c82d
Robert Beran <robert.beran@die-grafiken.de>
2019-04-02 21:44:36 +0200

    Update ProxyBrokerSort.php

--- ProxyBrokerSort.php
+++ ProxyBrokerSort.php
@@ -74,12 +74,3 @@ class ProxyBrokerSort {
     }
  
 }
- 
-$inst = new ProxyBrokerSort();
- 
-$inst->get_file("proxies/liste");
- 
-$inst->get_time();
-$inst->get_proxy_ip();
-$inst->get_proxy_port();
-$inst->save_to_file();
